5a1ddb7f14a85fef22284f906b197bd8ee03a68c
KittikarnIwai <kittigan1486@gmail.com>
2023-12-05 22:19:07 +0700

    Update Code

--- Web/script.js
+++ Web/script.js
@@ -4,4 +4,13 @@ function DarkMode() {
     element.classList.toggle("dark-mode");
     if (textbutton.innerHTML === ('Light')) textbutton.innerHTML = ('Dark')
     else textbutton.innerHTML = ('Light')
- }
+}
+
+function navbarmenu() {
+    var x = document.getElementById("item-navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
