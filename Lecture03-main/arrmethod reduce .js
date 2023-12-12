eee5ac16ff747394598595a6d89cd7c57f5fa40d
KittikarnIwai <kittigan1486@gmail.com>
2023-12-13 00:22:34 +0700

    Update Code

--- Lecture03-main/arrmethod reduce .js
+++ Lecture03-main/arrmethod reduce .js
@@ -1,7 +1,9 @@
-const array = [15,16,17,18,19]
+const array = [15, 16, 17, 18, 19];
 
-function reducer(accumulator , currentValue , index){}
-const returns = accumulator + currentValue;
-console.log(
-    `accumulator : ${}`
-)
+function reducer(accumulator, currentValue, index) {
+  return accumulator + currentValue;
+}
+
+const result = array.reduce(reducer, 0);
+
+console.log(`accumulator: ${result}`);
