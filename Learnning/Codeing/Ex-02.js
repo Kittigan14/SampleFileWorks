d93de1be2784bba49ea94c18348434bba0598dc5
Kittigan14 <kittigan1486@gmail.com>
2023-12-13 16:22:18 +0700

    Update Code

---
+++ Learnning/Codeing/Ex-02.js
@@ -0,0 +1,13 @@
+// equation("1+1");
+// equation("1+1+1+1+1");
+// // equation("7*4-2");
+
+// function equation (value) {
+//     const numbers = value.split('+');
+//     let result = 0;
+
+//     for (let count = 0; count < numbers.lenght; count++) {
+//         result += Number(numbers[count]);
+//     }
+//     console.log(result);
+// }
