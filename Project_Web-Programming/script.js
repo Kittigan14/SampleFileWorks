83822f5756b500c2e060009cec36125c29d9c5a4
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2023-12-15 23:26:49 +0700

    Update Code

--- Project_Web-Programming/script.js
+++
@@ -1,8 +0,0 @@
-function mynavbar() {
-    var navbar = document.getElementById("nav-bar");
-    if (navbar.className === "navbar") {
-        navbar.className += " responsive";
-    } else {
-        navbar.className = "navbar";
-    }
-}
