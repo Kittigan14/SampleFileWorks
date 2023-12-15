5e20268da7c41e1ab9c9640ee5b23616784b639c
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2023-12-15 18:14:19 +0700

    Update Code

--- Learnning/findpos.js
+++ Learnning/findpos.js
@@ -12,26 +12,26 @@ findPos(pos5);
 
 function findPos(pos) {
     const data = Array(41);
-    let datamove = 31;
+    let posmove = 31;
     data[31] = 'user';
 
     for (let i = 0; i < pos.length; i += true) {
         let position = pos[i].toUpperCase();
         if (position === 'N') {
-            data.splice(datamove, 1);
-            data[datamove -= 7] = 'user';
+            data.splice(posmove, 1);
+            data[posmove -= 7] = 'user';
 
         } else if (position === 'E') {
-            data.splice(datamove, 1);
-            data[datamove += 1] = 'user';
+            data.splice(posmove, 1);
+            data[posmove += 1] = 'user';
 
         } else if (position === 'W') {
-            data.splice(datamove, 1);
-            data[datamove -= 1] = 'user';
+            data.splice(posmove, 1);
+            data[posmove -= 1] = 'user';
 
         } else if (position === 'S') {
-            data.splice(datamove, 1);
-            data[datamove += 7] = 'user';
+            data.splice(posmove, 1);
+            data[posmove += 7] = 'user';
         }
     }
 
@@ -44,5 +44,5 @@ function findPos(pos) {
         ].includes(direction))) console.log('GAME OVER!, YOU FOUL!!!');
     else if (data[13] == 'user') console.log('Last Position : TG1 , You WIN!');
     else if (data[14] == 'user') console.log('Last Position : TG2 , You WIN!');
-    else console.log(`Last Position : ${datamove}  , You LOSE! :()`);
+    else console.log(`Last Position : ${posmove}  , You LOSE! :()`);
 }
