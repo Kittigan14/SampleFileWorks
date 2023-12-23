d8d13ae6d7794d443ec0df8ace74c2811c5b9a18
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2023-12-24 02:36:49 +0700

    Update Code

--- Learnning/Codeine/findMissingDigits.js
+++ Learnning/Codeine/findMissingDigits.js
@@ -4,12 +4,15 @@ function findMissingDigits(num) {
 
     for (let digit of digits) num.includes(digit) ? '' : result += digit + ', ';
 
-    if (result !== "") return num + ' Missing Digit = ' + result.slice(0, -2);
-    else return num + ' has no missing digits';
+    if (result !== '') return `${num} Missing Digit =  ${result.slice(0, -2)}`;
+    else return `${num} has no missing digits`;
 
 }
 
+const NumberStr1 = '123456789';
+const NumberStr2 = '53657687691428890';
+const NumberStr3 = '19810235689';
 
-console.log(findMissingDigits("123456789"));
-console.log(findMissingDigits("53657687691428890"));
-console.log(findMissingDigits("19810235689"));
+console.log(findMissingDigits(NumberStr1));
+console.log(findMissingDigits(NumberStr2));
+console.log(findMissingDigits(NumberStr3));
