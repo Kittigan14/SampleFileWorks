a6dfdb48de8c6e22d6a2483d28abe78a5fac724e
KittikarnIwai <kittigan1486@gmail.com>
2023-12-26 19:24:09 +0700

    Update Code

--- Learnning/Codeine/findCombinationOFWords.js
+++ Learnning/Codeine/findCombinationOFWords.js
@@ -1,4 +1,4 @@
-function findWords(words, characters) {
+function findCombinaryOfWords(words, characters) {
   const validWords = words.filter(word => {
     return word.split('').every(char => characters.includes(char));
   });
@@ -19,23 +19,23 @@ function findWords(words, characters) {
 const words = ["cherry", "date",  "honeydew",   "orange", "papaya"];
 const characters = ['o', 'r', 'a', 'n', 'g', 'e', 'h', 'y', 'd', 'w', 't'];
 
-const result = findWords(words, characters);
+const result = findCombinaryOfWords(words, characters);
 console.log(result);
 
 const words2 = ["date","fig","mango"];
 const characters2 = ['a', 't', 'e', 'm', 'n', 'g', 'o', 'f', 'i'];
 
-const result2 = findWords(words2, characters2);
+const result2 = findCombinaryOfWords(words2, characters2);
 console.log(result2);
 
 
 const words3 = ["apple", "banana", "orange","kiwi"];
 const characters3 = ['a', 'p', 'l', 'e', 'o', 'r', 'n', 'g'];
 
-const result3 = findWords(words3, characters3);
+const result3 = findCombinaryOfWords(words3, characters3);
 console.log(result3);
 
 
 module.exports = { 
-  findWords
+  findCombinaryOfWords
 };
