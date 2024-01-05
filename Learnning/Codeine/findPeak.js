309cd3bd082c2bbba91abc5ca2e6a2f2a2f31dfe
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-01-05 19:27:02 +0700

    Update Code

---
+++ Learnning/Codeine/findPeak.js
@@ -0,0 +1,13 @@
+function findAllPeakElements(nums) {
+    
+}
+
+const nums1 = [3, 2, 3, 1];
+const nums2 = [1, 2, 1, 3, 5, 6, 4];
+const nums3 = [1, 2, 1, 4, 5, 6, 4, 7];
+
+console.log(`Peak element index of : ${nums1} -> ${findAllPeakElements(nums1)}`);
+console.log(`Peak element index of : ${nums2} -> ${findAllPeakElements(nums2)}`);
+console.log(`Peak element index of : ${nums3} -> ${findAllPeakElements(nums3)}`);
+
+module.exports = findAllPeakElements;
