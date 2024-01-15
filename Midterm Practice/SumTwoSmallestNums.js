8d859afb08b2defa0e292643d0a38f06741af396
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-01-16 00:26:40 +0700

    Update Code

--- Midterm Practice/SumTwoSmallestNums.js
+++ Midterm Practice/SumTwoSmallestNums.js
@@ -5,7 +5,7 @@ function sumTwoSmallestNums(arr) {
     console.log(sum);
 }
 
-sumTwoSmallestNums([19 , 5 , 42 , 2 , 77]);                                         // Output: 7
-sumTwoSmallestNums([10 , 343445353 , 3453445 , 3453545353453]);                     // Output: 3453455
-sumTwoSmallestNums([879 , 953 , 694 , -847 , 342 , 221 , -91 , -723 , 791 , -587]); // Output: 563
-sumTwoSmallestNums([3683 , 2902 , 3951 , -475 , 1617 , -2385]);                     // Output: 4519
+sumTwoSmallestNums([19, 5, 42, 2, 77]); // Output: 7
+sumTwoSmallestNums([10, 343445353, 3453445, 3453545353453]); // Output: 3453455
+sumTwoSmallestNums([879, 953, 694, -847, 342, 221, -91, -723, 791, -587]); // Output: 563
+sumTwoSmallestNums([3683, 2902, 3951, -475, 1617, -2385]); // Output: 4519
