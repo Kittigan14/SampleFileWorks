76366d997a4c8af68a4d439d77a7bbb11fd0f1d0
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-01-20 21:14:34 +0700

    Update Code Min_Max

--- Learnning/Codeine/Min_Max.js
+++ Learnning/Codeine/Min_Max.js
@@ -1,31 +1,40 @@
-const rl = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
+// const rl = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// let A = 0;
+// let B = 0;
+
+// rl.on("line", (input) => {
+//   if (!A) A = parseInt(input);
+//   else {
+//     B = parseInt(input);
+
+//     let result = '';
+
+//     result = (A > B) ? result = "A" : result = "B";
+//     if (A === B) result = "AB";
+
+//     console.log(result);
+//     rl.close();
+//   }
+// });
+// rl.on("close", () => {});
+
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter Number 1 : ', (num1) => {
+  rl.question('Enter Number 2 : ', (num2) => {
+
+    let values = '';
+    num1 > num2 ? values = "A" : values = "B";
+    if (num1 === num2) values = "AB";
+
+    console.log(values);
+    rl.close();
   });
-  
-  let A = 0;
-  let B = 0;
-  
-  rl.on("line", (input) => {
-    if (!A) {
-      A = parseInt(input);
-    } else {
-      B = parseInt(input);
-  
-      let result = '';
-      if (A > B) {
-        result = 'A';
-      } else if (A < B) {
-        result = 'B';
-      } else {
-        result = 'AB';
-      }
-  
-      console.log(result);
-  
-      rl.close();
-    }
-  });
-  
-  rl.on("close", () => {
-  });
+});
