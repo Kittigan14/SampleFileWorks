202faf7de260ff57352e1cc32a4f0d7cfb04799e
KittikarnIwai <s6506021622060@email.kmutnb.ac.th>
2024-02-24 22:27:55 +0700

    Update Code

--- Web/ProjectJavaScript/app.js
+++ Web/ProjectJavaScript/app.js
@@ -1,33 +1,78 @@
 const express = require('express');
-const axios = require('axios');
 const path = require('path');
-var bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 
-const base_url = "http://localhost:3000";
-
-app.set('views', path.join(__dirname, '/public/views'));
-app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
 
-app.use(express.static(__dirname + '/public'));
+// Connection Database to File movieReviewsSystem.db
+const db = new sqlite3.Database('movieReviewsSystem.db');
 
-app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/index.html'));
+// Create Table Database
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Users (
+      UsersID INTEGER PRIMARY KEY AUTOINCREMENT,
+      UserName TEXT,
+      Email TEXT,
+      Password TEXT
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Movies (
+      MoviesID INTEGER PRIMARY KEY AUTOINCREMENT,
+      Title TEXT,
+      ReleaseDate DATE,
+      Genres TEXT,
+      Rating REAL
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Reviews (
+      ReviewsID INTEGER PRIMARY KEY AUTOINCREMENT,
+      UsersID INTEGER,
+      MovieID INTEGER,
+      Content TEXT,
+      TimeStamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY(UsersID) REFERENCES Users(UsersID),
+      FOREIGN KEY(MovieID) REFERENCES Movies(MoviesID)
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS Genres (
+      GenresID INTEGER PRIMARY KEY AUTOINCREMENT,
+      Name TEXT
+    )
+  `);
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/views/index.html'));
 });
 
-app.get('/login', async (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/Login.html'));
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/views/Register.html'));
 });
 
-app.get('/register', async (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/views/Register.html'));
+// Insert Users in Database, Table Uesrs
+app.post('/register', (req, res) => {
+    const data = req.body;
+    const sqlInsert = "INSERT INTO Users (Username, Email, Password) VALUES (?, ?, ?)";
+    db.run(sqlInsert, (data.Username, data.Email, data.Password), function (err) {
+            if (err) {
+                return console.error(err.message);
+            }
+
+            console.log(`A row has been inserted with rowid ${this.lastID}`);
+            res.sendFile(path.join(__dirname, "/public/views/index.html"));
+            // const thanksFilePath = path.join(__dirname, "/public/views/index.html");
+        });
 });
 
+// Create Severs
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
